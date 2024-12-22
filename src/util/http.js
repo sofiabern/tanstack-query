5f4038330390aaf68a1ac8105c07a59ad06aaf68
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-12-23 01:38:30 +0200

    useMutate for post

--- src/util/http.js
+++ src/util/http.js
@@ -1,3 +1,6 @@
+import { QueryClient } from "@tanstack/react-query";
+export const queryClient = new QueryClient();
+
 export async function fetchEvents({signal, searchTerm}) {
   let url = "http://localhost:3000/events";
   if (searchTerm) {
@@ -16,3 +19,40 @@ export async function fetchEvents({signal, searchTerm}) {
 
   return events;
 }
+
+export async function createNewEvent(eventData) {
+  const response = await fetch(`http://localhost:3000/events`, {
+    method: 'POST',
+    body: JSON.stringify(eventData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while creating the event');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
+
+export async function fetchSelectableImages({ signal }) {
+  const response = await fetch(`http://localhost:3000/events/images`, { signal });
+
+  if (!response.ok) {
+    const error = new Error('An error occurred while fetching the images');
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { images } = await response.json();
+
+  return images;
+}
+
