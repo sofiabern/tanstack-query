e1c44f44a80a031cb8045474dd5569dd06fb7c5e
Sofia Bern <128867240+sofiabern@users.noreply.github.com>
2024-12-23 02:21:39 +0200

    useMutation for delete

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,14 +1,31 @@
-import { Link, Outlet, useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { Link, Outlet, useParams, useNavigate } from "react-router-dom";
+import { useQuery, useMutation } from "@tanstack/react-query";
 import Header from "../Header.jsx";
 import ErrorBlock from "../UI/ErrorBlock";
-import { fetchEvent } from "../../util/http.js";
+import { fetchEvent, deleteEvent } from "../../util/http.js";
+import { queryClient } from "../../util/http.js";
 export default function EventDetails() {
   const params = useParams();
+  const navigate = useNavigate();
   const { data, isPending, isError, error } = useQuery({
     queryKey: ["events", params.id],
     queryFn: ({ signal }) => fetchEvent({ id: params.id, signal }),
   });
+
+  const { mutate } = useMutation({
+    mutationFn: deleteEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ['events'],
+        refetchType: 'none'
+      });
+      navigate("/events");
+    },
+  });
+
+  function handleDelete() {
+    mutate({ id: params.id });
+  }
 
   let content;
 
@@ -36,17 +53,16 @@ export default function EventDetails() {
 
   if (data) {
     const formattedDate = new Date(data.date).toLocaleDateString("en-US", {
-      day: 'numeric',
-      month: 'short',
-      year: 'numeric'
-    }
-    )
+      day: "numeric",
+      month: "short",
+      year: "numeric",
+    });
     content = (
       <>
         <header>
           <h1>{data.title}</h1>
           <nav>
-            <button>Delete</button>
+            <button onClick={handleDelete}>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
